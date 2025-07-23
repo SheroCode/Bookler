ac2fbd6fcd534efd58e9b29c9bf1ca2546b2b277
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-07-23 21:58:50 +0300

    refactor BookingPage component layout; remove commented-out code and enhance structure with HeaderBar, BookingFormCard, and SummaryCard

--- src/pages/BookingPage/BookingPage.jsx
+++ src/pages/BookingPage/BookingPage.jsx
@@ -1,8 +1,34 @@
+// import { useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+import HeaderBar from "../../components/HeaderBar/HeaderBar";
+import BookingFormCard from "../../components/BookingFormCard/BookingFormCard";
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import SummaryCard from "../../components/SummaryCard/SummaryCard";
 
 function BookingPage() {
+  // const { loggedIn } = useSelector((state) => state.use);
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (!loggedIn) {
+  //     navigate("/login");
+  //   }
+  // }, [loggedIn, navigate]);
+
   return (
-    <div>BookingPage</div>
-  )
+    <>
+      <HeaderBar />
+      <div className='flex flex-wrap my-5 px-4 gap-6'>
+        <div className='w-full lg:w-3/4'>
+          <BookingFormCard />
+        </div>
+        <div className='w-full lg:w-1/4'>
+          <SummaryCard />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default BookingPage
+export default BookingPage;
